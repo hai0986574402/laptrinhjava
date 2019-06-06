ca7b6655b259bb9fd5d8e1357573705f81259051
Thanh Hải <hai01666704375@gmail.com>
2019-06-06 14:08:51 +0700

    Tính tổng và TBC.

---
+++ HelloWorld/src/Mang/Tinhtong.java
@@ -0,0 +1,42 @@
+package Mang;
+
+import java.util.Scanner;
+
+public class Tinhtong {
+
+	public static void main(String[] args) {
+	    int n, sum = 0;
+	    float TBC;
+	    
+	    Scanner scanner = new Scanner(System.in);
+	         
+	    do {
+	        System.out.println("Nhập vào số phần tử của mảng: ");
+	        n = scanner.nextInt();
+	    } while (n < 0);
+	         
+	    int array[] = new int[n];
+	         
+	    System.out.println("Nhập các phần tử cho mảng: ");
+	    for (int i = 0; i < n; i++) {
+	        System.out.print("Nhập phần tử thứ " + i + ": ");
+	        array[i] = scanner.nextInt();
+	    }
+	         
+	    System.out.println("\nMảng ban đầu: ");
+	    for (int i = 0; i < n; i++) {
+	        System.out.print(array[i] + "\t");
+	    }
+	         
+	    for (int i = 0; i < n; i++) {
+	        sum += array[i];
+	    }
+	    System.out.println("\nTổng các phần tử có trong mảng = " + sum);
+	    
+	    TBC = sum / n;
+
+	    System.out.println ("trung binh cong cua mang:"+TBC);
+	    scanner.close();
+
+	}
+}
