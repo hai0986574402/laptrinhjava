ba2631b5a8b327540b347fe3bbde175701821c32
Thanh Hải <hai01666704375@gmail.com>
2019-06-13 17:41:26 +0700

    Kiểu 1

---
+++ HelloWorld/src/JavaHam/VD1.java
@@ -0,0 +1,17 @@
+package JavaHam;
+
+public class VD1 {
+	public static void Tamgiac()
+	{
+		System.out.println("  *");
+		System.out.println(" ***");
+		System.out.println("******");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tamgiac();
+
+	}
+
+}
