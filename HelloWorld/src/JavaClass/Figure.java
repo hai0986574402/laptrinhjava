80048b8f9a81148cb0b5cf7cf8483725b2406a64
Thanh Hải <hai01666704375@gmail.com>
2019-06-20 16:44:35 +0700

    Chu vi và Diện tích

---
+++ HelloWorld/src/JavaClass/Figure.java
@@ -0,0 +1,19 @@
+package JavaClass;
+
+public class Figure
+{
+	public double S, P;
+	public double calS()
+	{
+		return 0.0;
+	}
+	public double calP()
+	{
+		return 0.0;
+	}
+	public void output()
+	{
+		System.out.println("Diện tích:" +this.S);
+		System.out.println("Chu vi:" +this.P);
+	}
+}
